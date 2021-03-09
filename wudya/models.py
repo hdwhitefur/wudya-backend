805026292854a087689d53b14a2318da5e58c246
HD <hdwhitefur@gmail.com>
2021-03-09 14:55:45 -0600

    simplify option model

--- wudya/models.py
+++ wudya/models.py
@@ -3,8 +3,6 @@ from django.db import models
 
 class Option(models.Model):
     prompt = models.CharField(max_length=200)
-    votes_total = models.IntegerField(default=0)
-    other = models.ManyToManyField('self', through='OptionPair', symmetrical=True)
 
     def __str__(self):
         return self.prompt
